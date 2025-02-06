2213b2095886a2f6bfcdac203f745149b2cf3b79
AaronBDC <AaronBDC@mail.com>
2025-02-06 12:27:57 -0500

    Bank and BankTests added

---
+++ Bank/Bank/BankAccount.cs
@@ -0,0 +1,90 @@
+﻿//////// See https://aka.ms/new-console-template for more information
+//////Console.WriteLine("Hello, World!");
+using System;
+
+namespace BankAccountNS
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            //m_balance += amount; // intentionally incorrect code -- original (old)
+            m_balance -= amount; // intentionally incorrect code --  (new) works
+
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            /// @COMEBACK Remember this mistake ! - a few lines above in the Debit fx I was supposed to modify that line.
+            /// thats why my tests were broken and throwing this error below: SUCCESS!
+            /// 
+            ///
+            //////Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException
+            //////  HResult=0x80131500
+            //////  Message=Assert.AreEqual failed. Expected a difference no greater than <0.001> between expected value <7.44> and actual value <16.54>. Account not debited correctly
+            //////  Source=Microsoft.VisualStudio.TestPlatform.TestFramework
+            //////  StackTrace:
+            //////   at Microsoft.VisualStudio.TestTools.UnitTesting.Assert.ThrowAssertFailed(String assertionName, String message)
+            //////   at Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(Double expected, Double actual, Double delta, String message, Object[] parameters)
+            //////   at Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(Double expected, Double actual, Double delta, String message)
+            //////   at BankTests.BankAccountTests.Debit_WithValidAmount_UpdatesBalance() in C:\Users\nikkita\source\repos\Bank\BankTests\BankAccountTests.cs:line 28
+            //////   at System.RuntimeMethodHandle.InvokeMethod(Object target, Void** arguments, Signature sig, Boolean isConstructor)
+            //////   at System.Reflection.MethodBaseInvoker.InvokeWithNoArgs(Object obj, BindingFlags invokeAttr)
+
+            ///
+            //m_balance += amount; //old -- fails test
+            //m_balance -= amount; //new--fails: see above
+            m_balance += amount; //redid original for @lessons_learned: m_balance += amount;
+
+
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
---
+++ Bank/BankTests/BankAccountTests.cs
@@ -0,0 +1,59 @@
+﻿using BankAccountNS;
+
+
+// source: https://learn.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2022
+
+namespace BankTests
+{
+    [TestClass]
+    public sealed class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act and assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+    }
+}
+
+//////// example copied below:
+//////// The 'using' statement for Test Tools is in GlobalUsings.cs
+//////// using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//////namespace BankTests
+//////{
+//////    [TestClass]
+//////    public class BankAccountTests
+//////    {
+//////        [TestMethod]
+//////        public void TestMethod1()
+//////        {
+//////        }
+//////    }
+//////}
